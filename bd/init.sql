3369ebea97833245ede5dd49aaecb874048544e3
Abyssalnative <Abyssalnative@gmail.com>
2019-02-21 18:16:26 +0400

    edit init.sql

--- bd/init.sql
+++ bd/init.sql
@@ -1,5 +1,5 @@
 reate table lesson(
-id int AUTO_INCREMENT,
+id int SERIAL,
 name varchar NOT NULL,
 type varchar NOT NULL,
 teacher_id int NOT NULL,
@@ -8,7 +8,7 @@ FOREIGN KEY (teacher_id) REFERENCES teacher (id)
 );
 
 create table teacher (
-id int AUTO_INCREMENT,
+id int SERIAL,
 first_name varchar NOT NULL,
 last_name varchar NOT NULL,
 PRIMARY KEY (id)
@@ -19,7 +19,7 @@ even boolean NOT NULL,
 lesson_id int NOT NULL,
 pair_order int NOT NULL,
 day int NOT NULL,
-id int AUTO_INCREMENT,
+id int SERIAL,
 PRIMARY KEY (id),
 FOREIGN KEY (lesson_id) REFERENCES teacher (id)
 );
